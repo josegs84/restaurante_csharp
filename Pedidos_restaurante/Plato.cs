20d72e432fe1b34dff266150f5e42070863e2df0
Jose González <programacion2@akralogic.es>
2018-08-21 16:48:03 +0200

    Creando la clase Plato y definiendo cosas

---
+++ Pedidos_restaurante/Plato.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pedidos_restaurante
+{
+    class Plato
+    {
+        public Plato()
+        {
+
+        }
+        private int cantidad;
+
+        public string Contenido { get; set; }
+        public string Tamano_plato { get; set; }
+        public double Precio { get; set; }
+        public string Nombre { get; set; }
+
+        public int Cantidad
+        {
+            get { return Cantidad; }
+            set
+            {
+                cantidad = value;
+                if (cantidad <= 2)
+                    Tamano_plato = "Pequeño";
+                else if (cantidad > 2 & cantidad < 4)
+                    Tamano_plato = "Mediano";
+                else
+                    Tamano_plato = "Grande";
+            }
+        }
+
+        public void anadirCantidad()
+        {
+            Cantidad++;
+        }
+
+    }
+}
